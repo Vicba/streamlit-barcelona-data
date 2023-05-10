3fc1a1176f050df6bb50e7edea3dd7c0d90f0b37
victor.barra <victor.barra@student.howest.be>
2023-05-10 15:31:49 +0200

    chore: home page

--- project.py
+++ project.py
@@ -17,16 +17,27 @@ if dataset == "Home":
     # make header with picture
     st.image("./data/barcelona.jpg", width=700)
     st.title("Barcelona Data Sets")
-    st.write("This is a data set of Barcelona")
+
+    # subtitle
+    st.subheader("Introduction")
+    # intro text
+    st.write(
+        "Welcome to the Barcelona app, where you can explore and visualize various datasets related to the beautiful city of Barcelona. Whether you're a tourist planning a trip, a local resident interested in the city's demographics, or a data enthusiast, our app has something for you. Our dataset includes information on the city's neighborhoods, demographics, accommodations, and transportation. You can use our interactive visualizations to discover interesting trends and insights about Barcelona, such as the most popular neighborhoods for tourists or the average price of accommodations in different areas of the city. With the Barcelona app, you can plan your trip, learn more about the city, or satisfy your curiosity about its data."
+    )
 
 
 elif dataset == "Population":
     df = pd.read_csv("./data/population.csv")
     # delete page content
 
+
 elif dataset == "Unemployment":
     df = pd.read_csv("./data/unemployment.csv")
+
+
 elif dataset == "GDP":
     df = pd.read_csv("./data/gdp.csv")
+
+
 elif dataset == "Tourism":
     df = pd.read_csv("./data/tourism.csv")
