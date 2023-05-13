cad43f0c4c76366a4077ce6ed381a8ce14dc97f2
victor.barra <victor.barra@student.howest.be>
2023-05-13 22:13:25 +0200

    update tourism page

--- tourism.py
+++ tourism.py
@@ -67,3 +67,102 @@ def app():
     st_folium(m)
 
     #########################################################################################################################################################
+
+    st.write(
+        "List of music and drinks venues in the city of Barcelona that includes bars and pubs, cocktails, discotheques, karaokes, nightclubs, ballrooms, flamenco tablaos..."
+    )
+
+    # Load data from JSON file
+    url = "https://www.bcn.cat/tercerlloc/files/cultura/opendatabcn_cultura_espais-de-musica-i-copes-js.json"
+    data = pd.read_json(url)
+
+    # Select relevant columns and rename
+    data = data[["name", "geo_epgs_4326"]]
+    data = data.rename(columns={"geo_epgs_4326": "coordinates"})
+
+    # Extract latitude and longitude from coordinates
+    data["latitude"] = data["coordinates"].apply(lambda x: x["x"])
+    data["longitude"] = data["coordinates"].apply(lambda x: x["y"])
+
+    # Create map object
+    m = folium.Map(location=[41.3888, 2.159], zoom_start=13)
+
+    # Add markers for each hotel
+    for index, row in data.iterrows():
+        name = row["name"]
+        lat = row["latitude"]
+        lon = row["longitude"]
+        tooltip = f"{name}\n{name}"
+        marker = folium.Marker(location=[lat, lon], tooltip=tooltip)
+        marker.add_to(m)
+
+    # Display map in Streamlit app
+    folium_static(m)
+
+    #########################################################################################################################################################
+
+    st.write("Detail of main cultural interest points in the city of Barcelona.")
+
+    # Load data from JSON file
+    url = "https://www.bcn.cat/tercerlloc/files/opendatabcn_pics-js.json"
+    data = pd.read_json(url)
+
+    # Select relevant columns and rename
+    data = data[["name", "geo_epgs_4326"]]
+    data = data.rename(columns={"geo_epgs_4326": "coordinates"})
+
+    # Extract latitude and longitude from coordinates
+    data["latitude"] = data["coordinates"].apply(lambda x: x["x"])
+    data["longitude"] = data["coordinates"].apply(lambda x: x["y"])
+
+    # Create map object
+    m = folium.Map(location=[41.3888, 2.159], zoom_start=13)
+
+    # Add markers for each hotel
+    for index, row in data.iterrows():
+        name = row["name"]
+        lat = row["latitude"]
+        lon = row["longitude"]
+        tooltip = f"{name}\n{name}"
+        marker = folium.Marker(location=[lat, lon], tooltip=tooltip)
+        marker.add_to(m)
+
+    # Display map in Streamlit app
+    folium_static(m)
+
+    #########################################################################################################################################################
+
+    st.write("List and location of viewpoints in the city of Barcelona.")
+
+    # Load data from JSON file
+    url = "https://www.bcn.cat/tercerlloc/files/opendatabcn_pics-js.json"
+    data = pd.read_json(url)
+
+    # Select relevant columns and rename
+    data = data[["name", "geo_epgs_4326"]]
+    data = data.rename(columns={"geo_epgs_4326": "coordinates"})
+
+    # Extract latitude and longitude from coordinates
+    data["latitude"] = data["coordinates"].apply(lambda x: x["x"])
+    data["longitude"] = data["coordinates"].apply(lambda x: x["y"])
+
+    # Create map object
+    m = folium.Map(location=[41.3888, 2.159], zoom_start=13)
+
+    # Add markers for each hotel
+    for index, row in data.iterrows():
+        name = row["name"]
+        lat = row["latitude"]
+        lon = row["longitude"]
+        tooltip = f"{name}\n{name}"
+        marker = folium.Marker(location=[lat, lon], tooltip=tooltip)
+        marker.add_to(m)
+
+    # Display map in Streamlit app
+    folium_static(m)
+
+    #########################################################################################################################################################
+
+    st.write(
+        "Need to find more information about the city of Barcelona? Here are the locations of some tourist information offices. (IDK to add this because cant filter tourism in data.)"
+    )
